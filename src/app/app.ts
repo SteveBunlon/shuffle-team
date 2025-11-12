143c36156774ef9cabf7b68b01428f298a648832
stevebunlon <bunlon.steve@gmail.com>
2025-11-12 12:38:45 +0100

    fix error

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,4 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 import { Shuffle } from './shuffle/shuffle';
 
 @Component({
